1379e85a3766342653e7122d82c4a52809faeaa1
Edmar Felipe <edmarfelipe@outlook.com>
2024-08-24 16:39:06 -0300

    feat: add new router

---
+++ internal/httpserver/handler_hello.go
@@ -0,0 +1,29 @@
+package httpserver
+
+import "net/http"
+
+var (
+	msgs = map[string]string{
+		"en": "Hello",
+		"es": "Hola",
+		"pt": "Olá",
+	}
+)
+
+type HelloResponse struct {
+	Message string `json:"message"`
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	lang := r.URL.Query().Get("lang")
+	if lang == "" {
+		lang = "en"
+	}
+
+	if _, ok := msgs[lang]; !ok {
+		WriteJSON(w, http.StatusBadRequest, ErrorResponse{Error: "invalid language"})
+		return
+	}
+
+	WriteJSON(w, http.StatusOK, HelloResponse{Message: msgs[lang]})
+}
