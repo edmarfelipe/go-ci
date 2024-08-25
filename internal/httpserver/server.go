0801797ce23a53f0b2b4738269020af34223c939
Edmar Felipe <edmarfelipe@outlook.com>
2024-08-25 19:26:50 -0300

    chore: add HTTPServer Stop method for graceful shutdown

--- internal/httpserver/server.go
+++ internal/httpserver/server.go
@@ -1,6 +1,7 @@
 package httpserver
 
 import (
+	"context"
 	"log/slog"
 	"net/http"
 
@@ -24,7 +25,16 @@ func New(cfg *env.Env) *HTTPServer {
 	}
 }
 
+func (s *HTTPServer) Handler() http.Handler {
+	return s.srv.Handler
+}
+
 func (s *HTTPServer) Start() error {
 	slog.Info("starting http server", "addr", s.srv.Addr)
 	return s.srv.ListenAndServe()
+}
+
+func (s *HTTPServer) Stop(ctx context.Context) error {
+	slog.Info("stopping http server")
+	return s.srv.Shutdown(ctx)
 }
